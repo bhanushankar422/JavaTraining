81612fed57fbb141e51084546c800b96e601abc2
Bhanu P <bhannushankar422@gmail.com>
2020-06-16 01:50:35 -0500

    Added Non-primitive data types

---
+++ com/learning/elementary/NonPrimitiveDataTypes.java
@@ -0,0 +1,18 @@
+package com.learning.elementary;
+
+public class NonPrimitiveDataTypes {
+    public static void main(String[] args) {
+        String a1 = "Sample string 1";
+        String a2 = new String("Sample string 2");
+
+        Integer b1 = 12;
+        Integer b2 = 22;
+
+        System.out.println("a1: " + a1);
+        System.out.println("a2: " + a2);
+        System.out.println("b1: " + b1);
+        System.out.println("b2: " + b2);
+    }
+    
+
+}
--- com/learning/elementary/PrimitiveDataTypes.java
+++ com/learning/elementary/PrimitiveDataTypes.java
@@ -6,10 +6,14 @@ public class PrimitiveDataTypes{
         long c = 1000L;     
         float d = 2.4f;     
         double e = 7.5645;  
+        char letterZ = 'z';
+        boolean f = true;
 
         System.out.println("a: " + a );
         System.out.println("c: " + c );
         System.out.println("d: " + d );
         System.out.println("e: " + e );
+        System.out.println("letterZ: " + letterZ );
+        System.out.println("f: " + f );
     }
 }
