dc6ac49a856e71e3f9522ecf41e18d86eff33e69
Bhanu P <bhannushankar422@gmail.com>
2020-06-16 02:05:28 -0500

    Included arrays

--- com/learning/elementary/NonPrimitiveDataTypes.java
+++ com/learning/elementary/NonPrimitiveDataTypes.java
@@ -2,6 +2,9 @@ package com.learning.elementary;
 
 public class NonPrimitiveDataTypes {
     public static void main(String[] args) {
+        /*
+        * Immutable data types
+        */
         String a1 = "Sample string 1";
         String a2 = new String("Sample string 2");
 
@@ -12,6 +15,17 @@ public class NonPrimitiveDataTypes {
         System.out.println("a2: " + a2);
         System.out.println("b1: " + b1);
         System.out.println("b2: " + b2);
+
+        /*
+        * Reference types
+        */
+        String [] c = new String[3];
+        c[0] = "Apple";
+        c[1] = "Boy";
+        c[2] = "Cat";
+
+        System.out.println(c);
+        System.out.println(c[0] + ", " + c[1] + ", " + c[2]);
     }
     
 
