78e85aa8c6620513d72f7d9c3e99719ff59d68dd
Bhanu P <bhannushankar422@gmail.com>
2020-06-16 01:29:06 -0500

    Added PrimitiveDataType class

---
+++ com/learning/elementary/PrimitiveDataTypes.java
@@ -0,0 +1,15 @@
+package com.learning.elementary;
+
+public class PrimitiveDataTypes{
+    public static void main(String[] args) {
+        int a = 2;        
+        long c = 1000L;     
+        float d = 2.4f;     
+        double e = 7.5645;  
+
+        System.out.println("a: " + a );
+        System.out.println("c: " + c );
+        System.out.println("d: " + d );
+        System.out.println("e: " + e );
+    }
+}
