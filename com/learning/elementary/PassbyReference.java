d482fe0f41a5ebc79fbd54519671eca301c8dc8b
Bhanu P <bhannushankar422@gmail.com>
2020-06-16 22:48:07 -0500

    Included the immutable object call

--- com/learning/elementary/PassbyReference.java
+++ com/learning/elementary/PassbyReference.java
@@ -9,13 +9,28 @@ public class PassbyReference {
         System.out.println("Value of a before method call: " + passbyReference.a);
         passbyReference.passReference(passbyReference);
         System.out.println("Value of a after method call: " + passbyReference.a);
+
+        String tmp = new String("Bhanu");
+        System.out.println("Value of tmp before method call: " + tmp);
+        passbyReference.passImmutableObj(tmp);
+        System.out.println("Value of tmp after method call: " + tmp);
     }
 
     public void passReference(PassbyReference ref){
         System.out.println("Starting Value of ref: " + ref);
+        System.out.println("Starting Hashcode of ref: " + ref.hashCode());
         System.out.println("Starting Value of a: " + ref.a);
         ref.a = ref.a + 10;
         System.out.println("End Value of ref: " + ref);
+        System.out.println("Ending Hashcode of ref: " + ref.hashCode());
         System.out.println("End Value of a: " + ref.a);
+    }
+
+    public void passImmutableObj(String a){
+        System.out.println("Starting Value of a: " + a);
+        System.out.println("Starting Hashcode of a: " + a.hashCode());
+        a = "Hello! " + a;
+        System.out.println("End Value of a: " + a);
+        System.out.println("Ending Hashcode of a: " + a.hashCode());
     }
 }
