3980ddf2271e8cbbb21c49c751f79627a549d44c
Bhanu P <bhannushankar422@gmail.com>
2020-06-19 01:34:41 -0500

    Added word count example

---
+++ com/learning/highschool/collections/sample/IdentifyWordCount.java
@@ -0,0 +1,29 @@
+package com.learning.highschool.collections.sample;
+
+import java.util.HashMap;
+
+public class IdentifyWordCount {
+    public static void main(String[] args) {
+        IdentifyWordCount wc = new IdentifyWordCount();
+        String sentence = "I have a bike and that bike has a I symbol";
+        wc.printCountOfUniqueWords(sentence);
+    }
+
+    public void printCountOfUniqueWords(String sentence){
+        // splitting the sentence into words by using space as split character
+        String[] words = sentence.split(" ");
+        HashMap<String, Integer> wordCount = new HashMap<>();
+        for (String word : words) {
+            if(wordCount.get(word.toLowerCase()) == null){
+                wordCount.put(word.toLowerCase(), 1);
+            }else{
+                int count = wordCount.get(word.toLowerCase()) + 1;
+                wordCount.put(word, count);
+            }
+        }
+
+        for (String uniqueWord : wordCount.keySet()) {
+            System.out.println(uniqueWord + " -> " + wordCount.get(uniqueWord));
+        }
+    }
+}
