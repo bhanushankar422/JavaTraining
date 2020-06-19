c346b96f357eb1a25e7beda9c765a1025895a80e
Bhanu P <bhannushankar422@gmail.com>
2020-06-19 00:07:07 -0500

    Added arrays example

---
+++ com/learning/highschool/collections/TestArrays.java
@@ -0,0 +1,41 @@
+package com.learning.highschool.collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class TestArrays {
+    public static void main(String[] args) {
+        List<String> alphabets = new ArrayList<>();
+        alphabets.add("A");
+        alphabets.add("B");
+        alphabets.add("C");
+        alphabets.add("D");
+
+        //Regular for loop to iterate through the array list
+        System.out.println("****** Regular for loop **********");
+        for (int i = 0; i < alphabets.size(); i++) {
+            System.out.print(alphabets.get(i));
+            System.out.print(", ");
+        }
+        System.out.println("\n************************** ");
+
+        // Enhanced for loop
+        System.out.println("****** Enhanced for loop **********");
+        for (String alphabet : alphabets) {
+            System.out.print(alphabet);
+            System.out.print(", ");
+        }
+        System.out.println("\n************************** ");
+
+        // Iterator
+        System.out.println("****** Iterator **********");
+        Iterator itr = alphabets.iterator();
+        while(itr.hasNext()){
+            String alphabet = (String)itr.next();
+            System.out.print(alphabet);
+            System.out.print(", ");
+        }
+        System.out.println("\n************************** ");
+    }
+}
