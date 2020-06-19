99907004ba43f25ee0ff6e5da689ce83498749fb
Bhanu P <bhannushankar422@gmail.com>
2020-06-18 23:43:29 -0500

    Included abstract example

---
+++ com/learning/middleschool/abst/Child1.java
@@ -0,0 +1,15 @@
+package com.learning.middleschool.abst;
+
+public class Child1 extends Parent{
+    public void child1Method(String name){
+        System.out.println("\nHello! "+ name + ". This is exclusive Child1 method");
+    }
+
+    public void extendablePublicMethod(String name){
+        System.out.println("\nHello! "+ name + ". This is overriding extendablePublicMethod in  Child1 method");
+    }
+
+    protected void extendableProtectedMethod(String name){
+        System.out.println("\nHello! "+ name + ". This is overriding extendableProtectedMethod in  Child1 method");
+    }
+}
---
+++ com/learning/middleschool/abst/Child2.java
@@ -0,0 +1,7 @@
+package com.learning.middleschool.abst;
+
+public class Child2 extends Parent{
+    public void child2Method(String name){
+        System.out.println("\nHello! "+ name + ". This is exclusive Child2 method");
+    }
+}
